e057b57ada04b002015db9b339a65784229ac7eb
Ishant Ghawri <ishantghawri2002@gmail.com>
2024-07-09 11:35:37 +0530

    Updates

---
+++ precache-manifest.35f8ea33048fcc75bf143bc5eb483708.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7073afdc0e9656ae8d571a4c4f5eea5",
+    "url": "/TextUtils-React/index.html"
+  },
+  {
+    "revision": "a4dba2343b303ff9ca82",
+    "url": "/TextUtils-React/static/css/main.be0c93d8.chunk.css"
+  },
+  {
+    "revision": "586f3b0ff4ee5d99f9ed",
+    "url": "/TextUtils-React/static/js/2.969d2ac4.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/TextUtils-React/static/js/2.969d2ac4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a305264669ab0698eb9",
+    "url": "/TextUtils-React/static/js/3.a8bd2e92.chunk.js"
+  },
+  {
+    "revision": "a4dba2343b303ff9ca82",
+    "url": "/TextUtils-React/static/js/main.23f8d2bf.chunk.js"
+  },
+  {
+    "revision": "fd9a274372f88fe5cdd5",
+    "url": "/TextUtils-React/static/js/runtime-main.ab4f86cf.js"
+  }
+]);
